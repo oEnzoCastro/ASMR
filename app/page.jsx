f2e2c0c49405e437e11b37faef07652ef8804390
oEnzoCastro <enzocastru@gmail.com>
2025-02-04 20:29:15 -0300

    main

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import DiceRoll from "@components/DiceRoll";
+
+const Home = () => {
+  return (
+    <section className="DiceRollSection">
+      <DiceRoll></DiceRoll>
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/Nav.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+import Image from "next/image"
+
+const Nav = () => {
+  return (
+    <nav className="TopHeader">
+        <a href="/">ASMR</a>
+        <a href="Teste">SOBRE</a>
+        <a href="https://i.imgur.com/zjqIAOG.jpeg" target="_blank">MU/CORE</a>
+    </nav>
+  )
+}
+
+export default Nav
