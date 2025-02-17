6302c2b7b2005ba60d331146ff9877703365eaf9
oEnzoCastro <enzocastru@gmail.com>
2025-02-17 13:37:09 -0300

    Same as last

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Nav from "./components/Nav";
 import DiceRoll from "./components/DiceRoll";
 import NotFound from "./pages/NotFound";
 import Members from "./pages/Members";
-import Sobre from "./pages/sobre";
 import Simulador from "./pages/Simulador";
 
 function App() {
@@ -14,7 +13,7 @@ function App() {
       <Routes>
         <Route index element={<DiceRoll></DiceRoll>}></Route>
         <Route path="/Members" element={<Members></Members>}></Route>
-        <Route path="/Sobre" element={<Sobre></Sobre>}></Route>
+        <Route path="/Sobre" element={<Members></Members>}></Route>
         <Route path="/Simulador" element={<Simulador></Simulador>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
