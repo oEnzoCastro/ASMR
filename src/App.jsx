41ffde764c1ec67f6e19fbb02436e7e06cf2249a
oEnzoCastro <enzocastru@gmail.com>
2025-02-17 13:49:47 -0300

    Sobre is Back

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Nav from "./components/Nav";
 import DiceRoll from "./components/DiceRoll";
 import NotFound from "./pages/NotFound";
 import Members from "./pages/Members";
+import Sobre from "./pages/Sobre";
 import Simulador from "./pages/Simulador";
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
       <Routes>
         <Route index element={<DiceRoll></DiceRoll>}></Route>
         <Route path="/Members" element={<Members></Members>}></Route>
-        <Route path="/Sobre" element={<Members></Members>}></Route>
+        <Route path="/Sobre" element={<Sobre></Sobre>}></Route>
         <Route path="/Simulador" element={<Simulador></Simulador>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
